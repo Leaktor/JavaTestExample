d99e210faa0b245af154082b78bad56d33446344
Сергей Королев <leaktor@gmail.com>
2022-12-22 11:51:52 +0300

    v_01

--- src/test/java/steps/YandexStepdefs.java
+++ src/test/java/steps/YandexStepdefs.java
@@ -3,6 +3,9 @@ package steps;
 import Pages.CommonActions;
 import Pages.YandexMainPage;
 import io.cucumber.java.ru.Дано;
+import io.cucumber.java.ru.Если;
+import io.cucumber.java.ru.И;
+import io.cucumber.java.ru.Тогда;
 
 public class YandexStepdefs {
     YandexMainPage yandexMainPage = new YandexMainPage();
@@ -12,5 +15,20 @@ public class YandexStepdefs {
     @Дано("Открыть страницу {string}")
     public void открыть_страницу(String string) {
         commonActions.openPage(string);
+    }
+
+    @Если("Ввести {string} в строку поиска")
+    public void ввестиВСтрокуПоиска(String query) {
+        yandexMainPage.sendKeysInSearchString(query);
+    }
+
+    @И("Нажать кнопку Ввод")
+    public void нажатьКнопкуВвод() {
+        yandexMainPage.clickEnterButton();
+    }
+
+    @Тогда("Проверить что количество карточек в выдаче больше {int}")
+    public void проверитьЧтоКоличествоКарточекВВыдачеБольше(int cardsCount) {
+        yandexMainPage.cardInResultGreaterThan(cardsCount);
     }
 }
